f66965d31538615b0d1f8e84f0cf3786b10ec923
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-27 23:46:09 +0100

    will ADD: some styled components styles and repurposed vanilla css global styling into styled components way of doing global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+
+
+
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The wild oasis</H1>
+        <Button>Check in</Button>
+        <Input />
+        hello world
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
