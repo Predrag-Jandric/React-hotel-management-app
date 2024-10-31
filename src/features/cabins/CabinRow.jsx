1d3dc70c4310ed22781bc9f29d2155c81ae2b209
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-31 01:05:43 +0100

    will ADD: minor CabinTable.jsx content and CabinRow.jsx content, which is fetched from our database

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {formatCurrency} from "../../utils/helpers"
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +39,20 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinTable({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Firts up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Price>{formatCurrency(discount)}</Price>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinTable;
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
-import Spinner from "../../ui/Spinner"
+import Spinner from "../../ui/Spinner";
+import CabinRow from "../cabins/CabinRow"
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -28,15 +29,30 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-const {isLoading, data: cabins, error} = useQuery({
-  queryKey: ['cabin'],
-  queryFn: getCabins
-})
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
 
-if(isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
 
-
-  return <div>table</div>;
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin)=> <CabinRow cabin={cabin} key={cabin.id} />)}
+    </Table>
+  );
 }
 
-export default CabinTable
+export default CabinTable;
