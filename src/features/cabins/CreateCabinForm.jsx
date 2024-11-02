df8f98632c88c628ca52ab268ff0ab9e6295481a
predrag jandric <predrag.jandric.bg@gmail.com>
2024-11-02 23:15:46 +0100

    will ADD: feature of uploading an image inside create-cabin-form. image will be uploaded to supabase bucket storage

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -67,7 +67,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({...data, image: data.image[0]});
   }
 
   function onError(errors) {
@@ -147,7 +147,9 @@ function CreateCabinForm() {
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+        <FileInput type="file" id="image" accept="image/*" {...register("image", {
+            required: "This field is required",
+          })} />
       </FormRow>
 
       <FormRow>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://chwuyuowgztqpscswtyf.supabase.co";
+export const supabaseUrl = "https://chwuyuowgztqpscswtyf.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNod3V5dW93Z3p0cXBzY3N3dHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyNDM2MDcsImV4cCI6MjA0NTgxOTYwN30.OsW_K4hVGhcUDzzrsxTqJ10qWkaL8CnD_DJZYB358f8";
 const supabase = createClient(supabaseUrl, supabaseKey);
